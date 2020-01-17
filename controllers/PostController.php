10d1e27f82c643d2878bbd1ec669e921c834ad19
Ced59 <c.caudron59@gmail.com>
2020-01-17 09:46:13 +0100

    Routing List Comments ok
    *add  routing in PostController

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -4,10 +4,31 @@ namespace Valarep\controllers;
 
 use Valarep\View;
 use Valarep\objects\Post;
+use Valarep\Route;
+use Valarep\Router;
 
 
 class PostController
 {
+
+    public static function route()
+    {
+        $router = new Router();
+        $router->addRoute(new Route("/", "PostController", "ListAction"));
+        $router->addRoute(new Route("/posts", "PostController", "ListAction"));
+        $router->addRoute(new Route("/post/insert", "PostController", "PostAction"));
+
+        $route = $router->findRoute();
+
+        if ($route) {
+            $route->execute();
+        } else {
+            // Erreur 404
+
+            echo "Page Not Found";
+        }
+    }
+
     public static function ListAction()
     {
         // Route : /
@@ -18,8 +39,8 @@ class PostController
 
 
 
-    
-        
+
+
         View::setTemplate("post_list");
         View::bindVariable("posts", $posts);
         View::bindVariable("title", "Mon site");
@@ -33,5 +54,4 @@ class PostController
 
         Post::insertPost($title, $text);
     }
-
 }
